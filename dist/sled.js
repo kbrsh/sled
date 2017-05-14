3ce9be7e48e534d8f09614c081f91c206cbfb920
Gopal Shah <simplecooldude1@gmail.com>
2017-05-14 16:19:00 -0700

    init rewrite

--- dist/sled.js
+++ dist/sled.js
@@ -34,14 +34,6 @@
       // Setup Data
       this.data = [createVNode("p", "")];
     
-      // Selected Node Positions
-      this.nodeStart = 0;
-      this.nodeEnd = 0;
-    
-      // Cursor Positions
-      this.cursorStart = 0;
-      this.cursorEnd = 0;
-    
       // Set Content Editable
       this.el.setAttribute("contenteditable", "true");
     
@@ -60,11 +52,7 @@
       });
     
       this.el.addEventListener("keydown", function(e) {
-        // self.editAction(e);
-      });
-    
-      document.addEventListener("selectionchange", function(e) {
-        self.handleCursor(e);
+        self.editAction(e);
       });
     
       // Setup Initial Text
@@ -72,76 +60,11 @@
     }
     
     Sled.prototype.editText = function(text) {
-      var cursorStart = this.cursorStart;
-      var cursorEnd = this.cursorEnd;
-      var nodeStart = this.nodeStart;
-      var nodeEnd = this.nodeEnd;
     
-      var data = this.data;
-    
-      var node = data[nodeStart];
-      var content = node.content;
-    
-      if(cursorStart === cursorEnd) {
-        node.content = content.slice(0, cursorStart) + text + content.slice(cursorStart);
-      } else {
-        if(nodeStart === nodeEnd) {
-          node.content = content.slice(0, cursorStart) + text + content.slice(cursorEnd);
-        } else {
-          var nodeEndContent = data[nodeEnd].content;
-    
-          if(cursorStart === 0) {
-            node.content = text;
-          } else {
-            node.content = content.slice(0, cursorStart) + text;
-          }
-    
-          if(cursorEnd !== nodeEndContent.length) {
-            node.content += nodeEndContent.slice(cursorEnd);
-          }
-    
-          for(var i = nodeEnd; i < nodeStart; i++) {
-            data.splice(i, 1);
-          }
-        }
-      }
-    
-      this.updateIndex();
     }
     
-    Sled.prototype.handleCursor = function(e) {
-      var selection = document.getSelection();
+    Sled.prototype.editAction = function(e) {
     
-      var focusNode = selection.focusNode;
-    
-      if(focusNode !== null) {
-        var startNode = focusNode.parentNode;
-        var endNode = selection.anchorNode.parentNode;
-    
-        if(startNode.__sledIndex__ !== undefined) {
-          var start = selection.focusOffset;
-          var end = selection.anchorOffset;
-          if(end < start) {
-            var tmp = start;
-            start = end;
-            end = tmp;
-          }
-    
-          var startNodeIndex = startNode.__sledIndex__;
-          var endNodeIndex = endNode.__sledIndex__;
-          if(endNodeIndex < startNodeIndex) {
-            var tmp = startNodeIndex;
-            startNodeIndex = endNodeIndex;
-            endNodeIndex = tmp;
-          }
-    
-          this.cursorStart = start;
-          this.cursorEnd = end;
-    
-          this.nodeStart = startNodeIndex;
-          this.nodeEnd = endNodeIndex;
-        }
-      }
     }
     
     Sled.prototype.load = function(data) {
@@ -149,46 +72,8 @@
       this.build();
     }
     
-    Sled.prototype.updateIndex = function() {
-      var data = this.data;
-      var node = this.el.firstChild;
-    
-      for(var i = 0; i < data.length; i++) {
-        if(node.__sledIndex__ !== i) {
-          node.__sledIndex__ = i;
-        }
-      }
-    }
-    
     Sled.prototype.build = function() {
-      var el = this.el;
-      var data = this.data;
-      var node = el.firstChild;
     
-      for(var i = 0; i < data.length; i++) {
-        var vnode = data[i];
-    
-        if(node === null) {
-          node = createNode(vnode, i);
-          el.appendChild(node);
-        } else {
-          if(node.nodeName.toLowerCase() !== vnode.type) {
-            var oldNode = node;
-            node = createNode(vnode, i);
-            el.replaceChild(node, oldNode);
-          }
-    
-          if(i !== node.__sledIndex__) {
-            node.__sledIndex__ = i;
-          }
-    
-          if(vnode.content !== node.textContent) {
-            node.textContent = vnode.content;
-          }
-        }
-    
-        node = node.nextSibling;
-      }
     }
     
     Sled.version = "0.0.0";
--- src/index.js
+++ src/index.js
@@ -24,14 +24,6 @@ function Sled(el) {
   // Setup Data
   this.data = [createVNode("p", "")];
 
-  // Selected Node Positions
-  this.nodeStart = 0;
-  this.nodeEnd = 0;
-
-  // Cursor Positions
-  this.cursorStart = 0;
-  this.cursorEnd = 0;
-
   // Set Content Editable
   this.el.setAttribute("contenteditable", "true");
 
@@ -50,11 +42,7 @@ function Sled(el) {
   });
 
   this.el.addEventListener("keydown", function(e) {
-    // self.editAction(e);
-  });
-
-  document.addEventListener("selectionchange", function(e) {
-    self.handleCursor(e);
+    self.editAction(e);
   });
 
   // Setup Initial Text
@@ -62,76 +50,11 @@ function Sled(el) {
 }
 
 Sled.prototype.editText = function(text) {
-  var cursorStart = this.cursorStart;
-  var cursorEnd = this.cursorEnd;
-  var nodeStart = this.nodeStart;
-  var nodeEnd = this.nodeEnd;
 
-  var data = this.data;
-
-  var node = data[nodeStart];
-  var content = node.content;
-
-  if(cursorStart === cursorEnd) {
-    node.content = content.slice(0, cursorStart) + text + content.slice(cursorStart);
-  } else {
-    if(nodeStart === nodeEnd) {
-      node.content = content.slice(0, cursorStart) + text + content.slice(cursorEnd);
-    } else {
-      var nodeEndContent = data[nodeEnd].content;
-
-      if(cursorStart === 0) {
-        node.content = text;
-      } else {
-        node.content = content.slice(0, cursorStart) + text;
-      }
-
-      if(cursorEnd !== nodeEndContent.length) {
-        node.content += nodeEndContent.slice(cursorEnd);
-      }
-
-      for(var i = nodeEnd; i < nodeStart; i++) {
-        data.splice(i, 1);
-      }
-    }
-  }
-
-  this.updateIndex();
 }
 
-Sled.prototype.handleCursor = function(e) {
-  var selection = document.getSelection();
+Sled.prototype.editAction = function(e) {
 
-  var focusNode = selection.focusNode;
-
-  if(focusNode !== null) {
-    var startNode = focusNode.parentNode;
-    var endNode = selection.anchorNode.parentNode;
-
-    if(startNode.__sledIndex__ !== undefined) {
-      var start = selection.focusOffset;
-      var end = selection.anchorOffset;
-      if(end < start) {
-        var tmp = start;
-        start = end;
-        end = tmp;
-      }
-
-      var startNodeIndex = startNode.__sledIndex__;
-      var endNodeIndex = endNode.__sledIndex__;
-      if(endNodeIndex < startNodeIndex) {
-        var tmp = startNodeIndex;
-        startNodeIndex = endNodeIndex;
-        endNodeIndex = tmp;
-      }
-
-      this.cursorStart = start;
-      this.cursorEnd = end;
-
-      this.nodeStart = startNodeIndex;
-      this.nodeEnd = endNodeIndex;
-    }
-  }
 }
 
 Sled.prototype.load = function(data) {
@@ -139,46 +62,8 @@ Sled.prototype.load = function(data) {
   this.build();
 }
 
-Sled.prototype.updateIndex = function() {
-  var data = this.data;
-  var node = this.el.firstChild;
-
-  for(var i = 0; i < data.length; i++) {
-    if(node.__sledIndex__ !== i) {
-      node.__sledIndex__ = i;
-    }
-  }
-}
-
 Sled.prototype.build = function() {
-  var el = this.el;
-  var data = this.data;
-  var node = el.firstChild;
 
-  for(var i = 0; i < data.length; i++) {
-    var vnode = data[i];
-
-    if(node === null) {
-      node = createNode(vnode, i);
-      el.appendChild(node);
-    } else {
-      if(node.nodeName.toLowerCase() !== vnode.type) {
-        var oldNode = node;
-        node = createNode(vnode, i);
-        el.replaceChild(node, oldNode);
-      }
-
-      if(i !== node.__sledIndex__) {
-        node.__sledIndex__ = i;
-      }
-
-      if(vnode.content !== node.textContent) {
-        node.textContent = vnode.content;
-      }
-    }
-
-    node = node.nextSibling;
-  }
 }
 
 Sled.version = "__VERSION__";
